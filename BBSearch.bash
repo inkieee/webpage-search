85f9a30adc733cf901cb3a5a9d1d54ce130c5ce6
inkieee <inkieee@users.noreply.github.com>
2018-02-28 19:51:04 -0500

    Add files via upload

---
+++ BBSearch.bash
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+google-chrome "$https://www.bing.com/search?q=microsoft+search&FORM=AWRE&PC=U316"
+
+google-chrome  "$https://www.bing.com/search?q=fish&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=engineering+internship&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=mechanical&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=mechanical+internships&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=mechanical+engineering+jobs&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=job&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=jobs&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=engineering+opportunities&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=engineering+opportunity&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=mechanical+society&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=taco&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=puppies&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=burrito&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=grilling+technique&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=workout+tips&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=fitness&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=stretching&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=free+offer&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=freebie&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=science&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=strain+studies&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=jerk+chicken&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=3D+printer&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=python+script+examples&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=tools&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=matweb&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=wolframalpha&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=comsol&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=ANSYS&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=solidworks&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=matlab&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=autodesk&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=engineering+internship&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=mechanical&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=mechanical+internships&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=mechanical+engineering+jobs&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=job&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=jobs&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=engineering+opportunities&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=engineering+opportunity&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=mechanical+society&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=taco&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=puppies&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=burrito&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=grilling+technique&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=workout+tips&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=fitness&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=stretching&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=free+offer&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=freebie&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=science&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=strain+studies&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=jerk+chicken&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=3D+printer&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=python+script+examples&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=tools&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=matweb&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=wolframalpha&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=comsol&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=ANSYS&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=solidworks&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=matlab&go=Submit"
+
+google-chrome  "$https://www.bing.com/search?q=autodesk&go=Submit"
+
+chmod u+x BBSearch.sh
+
+
