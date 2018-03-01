c8bc09a169903ee486f7a252df1c8cf2e6abe277
inkieee <inkieee@users.noreply.github.com>
2018-02-28 21:52:27 -0500

    Add files via upload
    Bing search for windows

---
+++ b.bat
@@ -0,0 +1,214 @@
+﻿
+:: %1
+:: @echo off
+:: start microsoft-edge:"http://www.cnn.com/search/?text="%1%
+:: PING 127.0.0.1 -n 1 >NUL
+:: start /min /b microsoft-edge:"https://www.google.com/?gws_rd=ssl#q="%1%
+
+@echo off
+
+start chrome "https://www.bing.com/search?q=fish&go=Submit"
+PING 127.0.0.1 -n 1 -w 5000 >NUL
+taskkill /F /IM chrome.exe /T > nul
+
+start chrome "https://www.bing.com/search?q=fish&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=engineering+internship&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=mechanical&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=mechanical+internships&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=mechanical+engineering+jobs&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=job&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=jobs&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=engineering+opportunities&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=engineering+opportunity&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=mechanical+society&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=taco&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=puppies&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=burrito&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=grilling+technique&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=workout+tips&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=fitness&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=stretching&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=free+offer&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=freebie&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=science&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=strain+studies&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=jerk+chicken&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=3D+printer&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=python+script+examples&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=tools&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=matweb&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=wolframalpha&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=comsol&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=ANSYS&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=solidworks&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=matlab&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=autodesk&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+
+start chrome "https://www.bing.com/search?q=engineering+internship&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=mechanical&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=mechanical+internships&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=mechanical+engineering+jobs&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=job&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=jobs&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=engineering+opportunities&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=engineering+opportunity&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=mechanical+society&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=taco&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=puppies&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=burrito&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=grilling+technique&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=workout+tips&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=fitness&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=stretching&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=free+offer&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=freebie&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=science&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=strain+studies&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=jerk+chicken&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=3D+printer&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=python+script+examples&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=tools&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=matweb&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=wolframalpha&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=comsol&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=ANSYS&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=solidworks&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=matlab&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start chrome "https://www.bing.com/search?q=autodesk&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+
+
+:: close chrome, /T kills all child processes, > nul suppresses output/errors
+:: commenttttttttttt
+
+start microsoft-edge:"https://www.bing.com/search?q=engineering+internship&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=mechanical&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=mechanical+internships&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=mechanical+engineering+jobs&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=job&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=jobs&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=engineering+opportunities&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=engineering+opportunity&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=mechanical+society&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=taco&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=puppies&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=burrito&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=grilling+technique&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=workout+tips&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=fitness&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=stretching&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=free+offer&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=freebie&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=science&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=strain+studies&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=jerk+chicken&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=3D+printer&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=python+script+examples&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=tools&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=matweb&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=wolframalpha&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=comsol&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=ANSYS&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=solidworks&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=matlab&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+start microsoft-edge:"https://www.bing.com/search?q=autodesk&go=Submit"
+PING 127.0.0.1 -n 1 >NUL
+
+taskkill /F /IM MicrosoftEdge.exe /T 
+taskkill /F /IM chrome.exe /T > nul
+
+Exit /B 
+:: exit batch commands
+
